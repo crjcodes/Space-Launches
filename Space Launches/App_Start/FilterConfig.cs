d09c35e3e0dcd6947bf582efce85fcd199354cde
crjcodes <crjohnson@codeonward.com>
2020-06-09 12:43:03 -0700

    First ASP.NET demo

---
+++ Space Launches/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Space_Launches
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
